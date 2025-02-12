595ffe35743c9a062cf7d37abda39165987334e5
Enes <enesyenier98@gmail.com>
2025-02-12 13:19:21 +0300

    çıkarmada ilk sayının daha büyük olması

--- cikarma.js
+++ cikarma.js
@@ -1,1 +1,7 @@
  
+function cikar( {
+	if(parseInt(sayi1.value) < parseInt(sayi2.value))
+	    alert("çıkarma işlemi için birinci sayının ikinci sayıdan büyük olması gerekiyor!")
+	else
+		sonuc.innerHTML = parseInt(sayi1.value) - parseInt(sayi2.value);
+}
